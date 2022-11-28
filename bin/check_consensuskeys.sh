c0722259d024069af31fd6252b604813d2183c2a
Herr Seppia <seppia@dusk.network>
2022-11-28 13:21:21 +0100

    Improve installer script
    - Change the installer to preserve existing configuration
    - Change download location from DO to GH
    - Fix a warning while installing prerequisites
    - Add some logs to the standard output

--- bin/check_consensuskeys.sh
+++ bin/check_consensuskeys.sh
@@ -4,13 +4,13 @@ CONSENSUS_KEYS=`grep keysfile /opt/dusk/conf/dusk.toml | sed 's|.* "||g' | sed '
 if test -f "$CONSENSUS_KEYS"; then
     echo "Using CONSENSUS_KEYS in $CONSENSUS_KEYS"
 else 
-    echo "CONSENSUS_KEYS file not found in $CONSENSUS_KEYS"
+    echo "CONSENSUS_KEYS file not found in $CONSENSUS_KEYS" 1>&2
     exit 1
 fi
 
 if test -f "/opt/dusk/services/dusk.conf"; then
     exit 0
 else 
-    echo "CONSENSUS_KEYS password not found"
+    echo "CONSENSUS_KEYS password not set" 1>&2
     exit 1
 fi
