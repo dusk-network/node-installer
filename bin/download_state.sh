2249de492b5bb4b600e9b96aee1e7b0abe9efd20
HDauven <heindauven@gmail.com>
2024-03-12 10:44:46 +0100

    Update download_state
    - Add a list of available state
    - Default to first entry
    - When using an unavailable state, give list of satates

--- bin/download_state.sh
+++ bin/download_state.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+AVAILABLE_STATES=(409123 408251 407159 404345 379502)
+LATEST_STATE=${AVAILABLE_STATES[0]}
+
 # Function to display a warning message
 display_warning() {
   echo "WARNING: This operation will STOP your node and REPLACE the current state with a new one."
@@ -18,11 +21,30 @@ display_warning() {
   esac
 }
 
+list_states() {
+  echo "Available states:"
+  for state in "${AVAILABLE_STATES[@]}"; do
+    echo "- $state"
+  done
+  exit 0
+}
+
 # Check if an argument is provided, otherwise use the fallback value (348211)
 if [ $# -eq 0 ]; then
-  state_number=348211
+  # No argument provided, use the latest state
+  state_number=$LATEST_STATE
+elif [ "$1" = "--list" ]; then
+  # User requested to list all possible states
+  list_states
 else
+  # User provided a specific state
   state_number=$1
+  if ! [[ " ${AVAILABLE_STATES[*]} " =~ " ${state_number} " ]]; then
+    echo "Error: State $state_number is not available."
+    echo "The following states are available:"
+    list_states
+    exit 1
+  fi
 fi
 
 # Display warning and get user confirmation
@@ -31,7 +53,6 @@ display_warning
 # Download the file
 STATE_URL="https://nodes.dusk.network/state/$state_number"
 echo "Downloading state $state_number from $STATE_URL..."
-
 
 if ! curl -f -so  /tmp/state.tar.gz -L "$STATE_URL"; then
   echo "Error: Download failed. Exiting."
