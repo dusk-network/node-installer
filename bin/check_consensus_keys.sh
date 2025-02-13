854b5f71a38eb478ef353b2b0350e153edadc5bf
Herr Seppia <seppia@dusk.network>
2025-02-13 13:04:38 +0100

    simplifies `check_consensus_keys`
    See also dusk-network/rusk#3323

--- bin/check_consensus_keys.sh
+++ bin/check_consensus_keys.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 CONSENSUS_KEYS=`grep consensus_keys_path /opt/dusk/conf/rusk.toml | sed -n "s/^[^=]*= *'\([^']*\)'/\1/p"`
-if test -f "$CONSENSUS_KEYS"; then
-    echo "Using CONSENSUS_KEYS in $CONSENSUS_KEYS"
-else 
+if ! test -f "$CONSENSUS_KEYS"; then
     echo "CONSENSUS_KEYS file not found in $CONSENSUS_KEYS" 1>&2
     exit 1
 fi
 
-if test -f "/opt/dusk/services/dusk.conf"; then
-    exit 0
-else 
+if ! test -f "/opt/dusk/services/dusk.conf"; then
     echo "CONSENSUS_KEYS password not set" 1>&2
     exit 1
 fi
