fb2d66f3ea9988a0c4d451793d2fefe1667ac1de
Hein Dauven <heindauven@gmail.com>
2025-03-03 16:07:46 +0100

    Add mac os only support

--- node-installer.sh
+++ node-installer.sh
@@ -16,7 +16,7 @@ VERSIONS=(
 )
 
 # Default network and feature (Provisioner node)
-NETWORK="mainnet"
+NETWORK="devnet"
 FEATURE="default"
 
 # Parse command-line arguments to check for network or feature flags
@@ -32,7 +32,7 @@ while [[ "$#" -gt 0 ]]; do
             ;;
         *)
             echo "Unknown option: $1"
-            echo "Usage: $0 [--network mainnet|testnet] [--feature default|archive]"
+            echo "Usage: $0 [--network mainnet|testnet|devnet] [--feature default|archive]"
             exit 1
             ;;
     esac
@@ -40,7 +40,7 @@ done
 
 # Validate passed network
 case "$NETWORK" in
-    mainnet|testnet)
+    mainnet|testnet|devnet)
         echo "Selected network: $NETWORK"
         ;;
     *)
@@ -60,36 +60,27 @@ case "$FEATURE" in
         ;;
 esac
 
+# Platform validation
+if [[ "$(uname -s)" != "Darwin" ]]; then
+    echo "This installer only supports macOS."
+    exit 1
+fi
+
 # Retrieve architecture
 arch=""
 case "$(uname -m)" in
-    x86_64) arch="x64";;
-    aarch64) arch="arm64";;
-    *) echo "Unsupported architecture: $(uname -m). Only x64 and arm64 are supported."; exit 1;;
+    x86_64) arch="x64" ;;
+    arm64) arch="arm64" ;;
+    *) echo "Unsupported architecture: $(uname -m). Only x64 and arm64 are supported."; exit 1 ;;
 esac
 
-# Check for Linux
-if [ "$(uname -s | tr '[:upper:]' '[:lower:]')" != "linux" ]; then
-    echo "Unsupported platform: $(uname -s). This installer only supports Linux."
-    exit 1
-fi
-
-# Detect the Linux distribution
-distro="unknown"
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    distro=$(echo "$ID" | tr '[:upper:]' '[:lower:]')
-else
-    echo "Unable to detect the Linux distribution. Ensure this is an Ubuntu-based system."
-    exit 1
-fi
-
-if [ "$distro" != "ubuntu" ]; then
-    echo "Unsupported Linux distribution: $distro. This installer only supports Ubuntu."
-    exit 1
-fi
-
-echo "Detected supported distro: $distro ($arch)"
+# Check required macOS tools
+for pkg in curl unzip jq; do
+    if ! command -v $pkg >/dev/null 2>&1; then
+        echo "$pkg is required. Install it using Homebrew: brew install $pkg"
+        exit 1
+    fi
+done
 
 # Installs a given component for a given network
 install_component() {
@@ -115,7 +106,7 @@ install_component() {
     # Removes any RC version from the URL
     local sanitized_version="${version%-rc.*}" 
     # Construct the download URL
-    local url="https://github.com/dusk-network/rusk/releases/download/${release_tag}-${version}/${component}-${sanitized_version}-linux-${arch}${feature_suffix}.tar.gz"
+    local url="https://github.com/dusk-network/rusk/releases/download/${release_tag}-${version}/${component}-${sanitized_version}-macos-arm64${feature_suffix}.tar.gz"
 
     echo "Installing $component version $version for $network ($arch${feature_suffix})"
     echo "Downloading from $url"
@@ -125,31 +116,6 @@ install_component() {
 
     curl -so /opt/dusk/installer/${component}.tar.gz -L "$url" || { echo "Failed to download $component"; exit 1; }
     tar xf /opt/dusk/installer/${component}.tar.gz --strip-components 1 --directory "$component_dir"
-}
-
-# Check for OpenSSL 3 or higher
-if ! command -v openssl >/dev/null 2>&1 || [ "$(openssl version | awk '{print $2}' | cut -d. -f1)" -lt 3 ]; then
-    echo "The required OpenSSL version is not available. Please install OpenSSL 3 or higher"
-    echo "You likely need to upgrade your OS or install a newer OS"
-    exit 1
-fi
-
-update_pkg_database() {
-    echo "Updating package database..."
-    apt update
-}
-
-check_installed() {
-    binary_name=$1
-    package_name=$2
-
-    if ! which $binary_name >/dev/null 2>&1; then
-        echo "$binary_name missing"
-        echo "Installing $package_name"
-        NEEDRESTART_MODE=a apt install $package_name -y
-    else
-        echo "$binary_name is already installed."
-    fi
 }
 
 # Configure your local installation based on the selected network
@@ -163,6 +129,8 @@ configure_network() {
             mv /opt/dusk/conf/mainnet.toml /opt/dusk/conf/rusk.toml
             rm /opt/dusk/conf/testnet.genesis
             rm /opt/dusk/conf/testnet.toml
+            rm /opt/dusk/conf/devnet.genesis
+            rm /opt/dusk/conf/devnet.toml
             prover_url="https://provers.dusk.network"
             ;;
         testnet)
@@ -170,11 +138,22 @@ configure_network() {
             mv /opt/dusk/conf/testnet.toml /opt/dusk/conf/rusk.toml
             rm /opt/dusk/conf/mainnet.genesis
             rm /opt/dusk/conf/mainnet.toml
+            rm /opt/dusk/conf/devnet.genesis
+            rm /opt/dusk/conf/devnet.toml
             prover_url="https://testnet.provers.dusk.network"
             ;;
+        devnet)
+            mv /opt/dusk/conf/devnet.genesis /opt/dusk/conf/genesis.toml
+            mv /opt/dusk/conf/devnet.toml /opt/dusk/conf/rusk.toml
+            rm /opt/dusk/conf/mainnet.genesis
+            rm /opt/dusk/conf/mainnet.toml
+            rm /opt/dusk/conf/testnet.genesis
+            rm /opt/dusk/conf/testnet.toml
+            prover_url="https://devnet.provers.dusk.network"
+            ;;
         *)
-            echo "Unknown network: $network. Defaulting to mainnet."
-            configure_network "mainnet"
+            echo "Unknown network: $network. Defaulting to devnet."
+            configure_network "devnet"
             return
             ;;
     esac
@@ -183,42 +162,8 @@ configure_network() {
     sed -i "s|^prover = .*|prover = \"$prover_url\"|" $CURRENT_HOME/.dusk/rusk-wallet/config.toml
 }
 
-echo "Stopping previous services"
-if systemctl is-active --quiet rusk; then
-    systemctl stop rusk
-    echo "Stopped rusk service."
-else
-    echo "Rusk service not running."
-fi
-
 rm -rf /opt/dusk/installer || true
 rm -rf /opt/dusk/installer/installer.tar.gz || true
-
-echo "Checking prerequisites"
-update_pkg_database
-check_installed unzip unzip
-check_installed curl curl
-check_installed route net-tools
-check_installed ipcalc ipcalc
-check_installed jq jq
-check_installed logrotate logrotate
-
-# Ensure dusk group and user exist
-if ! id -u dusk >/dev/null 2>&1; then
-    echo "Creating dusk system user and group."
-    groupadd --system dusk
-    useradd --system --create-home --shell /usr/sbin/nologin --gid dusk dusk
-    echo "User 'dusk' and group 'dusk' created."
-else
-    echo "User 'dusk' and group 'dusk' already exist."
-fi
-
-# Add the current user to the dusk group for access
-echo "Adding current user to dusk group for access."
-if ! id -nG "$CURRENT_USER" | grep -qw "dusk"; then
-    usermod -aG dusk "$CURRENT_USER"
-    echo "User $CURRENT_USER has been added to the dusk group. Please log out and back in to apply changes."
-fi
 
 mkdir -p /opt/dusk/bin
 mkdir -p /opt/dusk/conf
@@ -238,7 +183,6 @@ tar xf /opt/dusk/installer/installer.tar.gz --strip-components 1 --directory /op
 # Handle scripts, configs, and service definitions
 mv -f /opt/dusk/installer/bin/* /opt/dusk/bin/
 mv /opt/dusk/installer/conf/* /opt/dusk/conf/
-mv -n /opt/dusk/installer/services/* /opt/dusk/services/
 
 # Download, unpack and install Rusk
 install_component "$NETWORK" "rusk"
@@ -261,7 +205,7 @@ fi
 echo "Downloading verifier keys"
 
 echo "Selected network: $NETWORK"
-VERIFIER_KEYS_URL="https://mainnet.nodes.dusk.network/keys"
+VERIFIER_KEYS_URL="https://devnet.nodes.dusk.network/keys"
 
 case "$NETWORK" in
     mainnet)
@@ -270,8 +214,11 @@ case "$NETWORK" in
     testnet)
         VERIFIER_KEYS_URL="https://testnet.nodes.dusk.network/keys"
         ;;
+    devnet)
+        VERIFIER_KEYS_URL="https://devnet.nodes.dusk.network/keys"
+        ;;
     *)
-        echo "Unknown network: $network. Defaulting to mainnet."
+        echo "Unknown network: $network. Defaulting to devnet."
         return
         ;;
 esac
@@ -290,23 +237,6 @@ chmod -R 660 /opt/dusk
 # Directory listing needs execution
 find /opt/dusk -type d -exec chmod +x {} \;
 chmod +x /opt/dusk/bin/*
-
-# Set system parameters
-mv -f /opt/dusk/conf/dusk.conf /etc/sysctl.d/dusk.conf
-sysctl -p /etc/sysctl.d/dusk.conf
-
-echo "Installing services"
-# Overwrite previous service definitions
-mv -f /opt/dusk/services/rusk.service /etc/systemd/system/rusk.service
-
-# Configure logrotate with 644 permissions otherwise configuration is ignored
-mv -f /opt/dusk/services/logrotate.conf /etc/logrotate.d/dusk.conf
-chown root:root /etc/logrotate.d/dusk.conf
-chmod 644 /etc/logrotate.d/dusk.conf
-
-# Enable the Rusk service
-systemctl enable rusk
-systemctl daemon-reload
 
 # Display final instructions
 cat /opt/dusk/installer/assets/finish.msg
