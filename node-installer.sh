1664e1880ccbb68c27a1725d376d4f396fc9848b
Herr Seppia <seppia@dusk.network>
2025-03-20 16:34:27 +0100

    Upgrade mainnet binary

--- node-installer.sh
+++ node-installer.sh
@@ -9,7 +9,7 @@ echo "Home directory: $CURRENT_HOME"
 declare -A VERSIONS
 # Define versions per network, per component
 VERSIONS=(
-    ["mainnet-rusk"]="1.1.1"
+    ["mainnet-rusk"]="1.2.0"
     ["mainnet-rusk-wallet"]="0.1.0-rc.0"
     ["testnet-rusk"]="1.2.0"
     ["testnet-rusk-wallet"]="0.1.0-rc.0"
