f48227d4473edd87f2db7b2da6274edeeff51af9
Herr Seppia <seppia@dusk.network>
2026-05-07 15:02:39 +0200

    Update to release 0.5.17

--- node-installer.sh
+++ node-installer.sh
@@ -11,8 +11,8 @@ declare -A VERSIONS
 VERSIONS=(
     ["mainnet-rusk"]="1.6.0"
     ["mainnet-rusk-wallet"]="0.3.0"
-    ["testnet-rusk"]="1.6.0"
-    ["testnet-rusk-wallet"]="0.3.0"
+    ["testnet-rusk"]="1.7.0-rc.0"
+    ["testnet-rusk-wallet"]="0.4.0"
 )
 
 # Default network and feature (Provisioner node)
@@ -156,7 +156,7 @@ chown -R "$CURRENT_USER:dusk" "$CURRENT_HOME/.dusk"
 chmod -R 770 "$CURRENT_HOME/.dusk"
 
 # Download and extract installer files
-INSTALLER_URL="https://github.com/dusk-network/node-installer/tarball/main"
+INSTALLER_URL="https://github.com/dusk-network/node-installer/archive/refs/tags/v0.5.17.tar.gz"
 echo "Downloading installer package for additional scripts and configurations"
 curl -so /opt/dusk/installer/installer.tar.gz -L "$INSTALLER_URL"
 tar xf /opt/dusk/installer/installer.tar.gz --strip-components 1 --directory /opt/dusk/installer
